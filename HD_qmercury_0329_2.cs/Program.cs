01942a482b7daee151258b6d2732e2c6c5298cd8
qmercury <qmercury@knou.ac.kr>
2022-03-30 15:24:25 +0900

    유니티스터디 0329 연습문제 2변

---
+++ HD_qmercury_0329_2.cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HD_qmercury_0329_2.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// while만 이용하여 2단, 4단, 6단 구구단을 출력하는 프로그램을 작성하기
+
+            int i = 2;
+            while (i < 7)
+            {
+                int j = 1;
+                while (j < 10)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                    ++j;
+                    
+                }
+                j = 1;
+                i = i + 2;
+            }
+            
+        }
+    }
+}
